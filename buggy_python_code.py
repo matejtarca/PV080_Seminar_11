03cb574d6096133291d7e4cd6dbde3e8fea51d67
unknown <xtarca@ad.fi.muni.cz>
2022-04-26 18:54:50 +0200

    fixpython

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -5,6 +5,11 @@ import subprocess
 import base64
 import flask
 
+DICT = {
+    'first': '1.3',
+    'second': '2.3'
+}
+
 # Input injection
 def transcode_file(request, filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
@@ -25,10 +30,13 @@ class RunBinSh(object):
 def import_urlib_version(version):
     exec("import urllib%s as urllib" % version)
 
+def sanitize(module):
+    return DICT.get(module, '3.0')
+    
 @app.route('/')
 def index():
     module = flask.request.args.get("module")
-    import_urlib_version(module)
+    import_urlib_version(sanitize(module))
 
 
 print(base64.b64encode(pickle.dumps(RunBinSh())))
